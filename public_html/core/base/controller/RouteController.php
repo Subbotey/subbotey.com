7dcf762b767a251ca99c6e68ef3e1ddd9fe908b2
Subbotey <subbotey@yandex.ru>
2021-03-01 12:19:02 +0300

    Sin

--- public_html/core/base/controller/RouteController.php
+++ public_html/core/base/controller/RouteController.php
@@ -2,6 +2,7 @@
 
 namespace core\base\controller;
 
+use core\base\exceptions\RouteException;
 use core\base\settings\Settings;
 use core\base\settings\ShopSettings;
 
@@ -32,7 +33,61 @@ class RouteController
     private function __construct()
     {
         $address_str = $_SERVER['REQUEST_URI'];
+        if (strrpos($address_str, '/') === strlen($address_str) - 1 && strrpos($address_str, '/') !== 0) {
+            $this->redirect(rtrim($address_str, '/'), 301);
+        }
 
-        exit();
+        $path = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php'));
+
+
+        if ($path === PATH) {
+            $this->routes =settings::get('routes');
+
+            if (!$this->routes) {
+                throw new RouteException('Сайт на обслуживании');
+            }
+            if (strrpos($address_str, $this->routes['admin']['alias']) === strlen(PATH)) {
+            } else {
+                $url = explode('/', substr($address_str, strlen(PATH)));
+
+                $hrUrl = $this->routes['user']['hrUrl'];
+
+                $this->controller = $this->routes['user']['path'];
+
+                $route = 'user';
+            }
+
+            $this->createRoute($route, $url);
+
+             exit();
+        } else {
+            try {
+                throw new \Exception('Не корректная директория сайта');
+            } catch (\Exception $e) {
+                exit($e->getMessage());
+            }
+        }
+    }
+
+    private function createRoute($var, $arr)
+    {
+        $route = [];
+
+        if (!empty($arr[0])) {
+            if ($this->routes[$var]['routes'][$arr[0]]) {
+                $route = explode('/', $this->routes[$var]['routes'][$arr[0]]);
+
+                $this->controller .= ucfirst($route[0] . 'Controller');
+            } else {
+                $this->controller .= ucfirst($arr[0] . 'Controller');
+            }
+        } else {
+            $this->controller .= $this->routes['default']['controller'];
+        }
+
+        $this->inputMethod = $route[1] ? $route[1] : $this->routes['default']['inputMethod'];
+        $this->outputMethod = $route[2] ? $route[2] : $this->routes['default']['outputMethod'];
+
+        return;
     }
 }
